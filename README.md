866429124af614dd10a19159c820e9dd4e8d0f94
stefan-baranoff <sbaranoff@gmail.com>
2020-08-01 02:06:55 +0000

    Initial commit of scraper

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # perl-scraper
+On a RHEL/CentOS 7/8 equivalent system you can yum/dnf install these to get the required packages. It should work on 6, too...
+perl-LWP-Protocol-https
+perl-JSON
---
+++ perl-scraper.pl
@@ -0,0 +1,246 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+use LWP::UserAgent;
+use Digest::SHA 'sha256_hex';
+use MIME::Base64;
+use JSON;
+use POSIX 'strftime';
+
+# Read lines from input file into array
+sub read_file_lines {
+  my $filename = shift;
+  my $fh = IO::File->new();
+  unless ($fh->open($filename, "r")) {
+    die "Could not open [ $filename ] for reading: $!\n";
+  }
+  my $lines;
+  while (<$fh>) {
+    chomp($_);
+    push(@{$lines}, $_);
+  }
+  return $lines;
+}
+
+# Get a random source port to use for each request, so we know what it is
+sub get_sport {
+  return int(rand(64511)) + 1024;
+}
+
+# A list of user-agents to randomly rotate through
+my @USER_AGENTS = (
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0",
+  "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:79.0) Gecko/20100101 Firefox/79.0",
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 10_15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/28.0 Mobile/15E148 Safari/605.1.15",
+  "Mozilla/5.0 (Android 10; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+  "Mozilla/5.0 (iPad; CPU OS 13_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/83.0.4147.71 Mobile/15E148 Safari/604.1",
+  "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Mobile Safari/537.36",
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+);
+sub get_random_ua_string {
+  my $idx = int(rand(scalar(@USER_AGENTS)));
+  return $USER_AGENTS[$idx];
+}
+
+# Some "referers" to randomly put into the headers
+my @REFERERS = (
+  "https://www.google.com/",
+  "https://www.yahoo.com/",
+  "https://www.duckduckgo.com/",
+);
+# If we only want to randomly add a referer and not always have one, reduce this number
+my $REFERER_PERCENTAGE = 1.0;
+sub get_referer {
+  my $referer_chance = rand();
+  if ($referer_chance <= $REFERER_PERCENTAGE) {
+    my $idx = int(rand(scalar(@REFERERS)));
+    return $REFERERS[$idx];
+  }
+  return undef;
+}
+
+# Given a URL, make an HTTP::Request object to request it with a GET
+# Adds a DNT header, an Accept header, Accept-Encoding for gzip if support, and a random referer
+# User-agent is handled via the LWP::UserAgent->agent(...) call
+sub get_request {
+  my $url = shift;
+
+  my $request = HTTP::Request->new('GET', $url, []);
+  my $referer = get_referer();
+  if ($referer) {
+    $request->header('Referer', $referer);
+  }
+  $request->header('DNT', 1);
+  $request->header('Accept', 'text/html,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9');
+
+  # Borrowed from HTTP::Message.pm -- we don't want to advertize everything, just gzip if we can support it
+  {
+    local $@;
+    eval {
+      require IO::Uncompress::Gunzip;
+      $request->header('Accept-Encoding', 'gzip');
+    };
+  }
+
+  return $request;
+}
+
+# Actually make an HTTP request
+# Returns a list of the request object, the response object, and the source port used
+# This is NOT using a connection cache so as to be able to provide the source port
+# used for the request; there's no clean way to get that otherwise. 
+sub make_http_request {
+  my $url = shift;
+
+  my $ua = LWP::UserAgent->new();
+  $ua->agent(get_random_ua_string());
+  my $request = get_request($url);
+
+  my $port = get_sport();
+  $ua->local_address("0.0.0.0:${port}");
+
+  my $response = $ua->request($request);
+  return ($request, $response, $port);
+}
+
+# Convert an HTTP::Headers object into a structured K/V store
+# Multi-value headers are possible so all entries are stored in arrays
+# HTTP::Headers does not provide 'raw' data, it only allows 'normalized'
+# name values and does not preserve order - it's designed around being
+# 'friendly' for the developer and not forensic.
+#
+# This is good enough to triage with existing capabilities, but is NOT
+# diagnostic or forensic in nature! Take the names/values with a grain of salt.
+sub get_headers_map {
+  my $headers = shift;
+  my $header_map = {};
+  $headers->scan(sub {
+    my $name = shift;
+    my $value = shift;
+    push(@{$header_map->{$name}}, $value);
+  });
+  return $header_map;
+}
+
+sub ensure_outdir {
+  my $outdir = shift;
+  if (not -w $outdir) {
+    die "Output directory [ $outdir ] is not writable, pick an appropriate output directory and try again.\n";
+  }
+}
+
+my $usage = <<EOL;
+Usage: $0
+    -u, --url-list
+        A file containing a list of URLs to grab, one per line, starting with the scheme.
+        Example file contents:
+          https://www.example.com/
+          http://www.example.com/second_file.html
+          https://www.example.com/this/is/a/full/path
+          https://www.example.com:8443/non/stanard/port
+    -o, --out-dir
+        The directory, which must already exist and be writable, to output results into.
+    -a, --user-agents [OPTIONAL]
+        A file containing a list of User-Agent values to randomly select from, one per line.
+        If not provided a default build-in list is used.
+    -r, --referers [OPTIONAL]
+        A file containing a list of Referer values to randomly select from, one per line.
+        If not provided a default build-in list is used.
+EOL
+
+sub main {
+  # Parse arguments
+  my $url_list_file;
+  my $out_dir;
+  my $ua_file;
+  my $ref_file;
+  GetOptions(
+    'url-list|u=s' => \$url_list_file,
+    'out-dir|o=s' => \$out_dir,
+    'user-agents|a:s' => \$ua_file,
+    'referers|r:s' => \$ref_file)
+  or die $usage;
+
+  if (not defined $url_list_file) {
+    die("-u, --url-list is required but not supplied!\n$usage");
+  }
+  if (not defined $out_dir) {
+    die("-o, --out-dir is required but not supplied!\n$usage");
+  }
+
+  my $urls = read_file_lines($url_list_file);
+  if (defined($ua_file)) {
+    @USER_AGENTS = @{read_file_lines($ua_file)};
+  }
+  if (defined($ref_file)) {
+    @REFERERS = @{read_file_lines($ref_file)};
+  }
+
+  ensure_outdir($out_dir);
+
+  my @urls = @{read_file_lines($url_list_file)};
+  my $url_index = 0;
+  foreach my $url (@urls) {
+    my $start_time_string = strftime("%FT%T%Z", gmtime());
+    my ($request, $response, $local_port) = make_http_request($url);
+    my $stop_time_string = strftime("%FT%T%Z", gmtime());
+
+    my $request_headers_map = get_headers_map($request->headers());
+    my $response_headers_map = get_headers_map($response->headers());
+    # charset => none SHOULD return a byte stream (result of Encode)
+    # and only 'decode' transfer encoding (gzip, chunk, etc.) and not charset
+    my $content = $response->decoded_content('charset' => 'none');
+    my ($dest_addr, $dest_port) = split(/:([0-9]+)$/, $response->header('Client-Peer'));
+
+    # Create metadata about this download
+    my $result = {
+      'start_timestamp' => $start_time_string,
+      'stop_timestamp' => $stop_time_string,
+      'url' => $url,
+      'source_port' => $local_port,
+      'destination_address' => $dest_addr,
+      'destination_port' => $dest_port,
+      'request_headers' => $request_headers_map,
+      'response_status' => $response->code(),
+      'response_status_message' => $response->message(),
+      'response_headers' => $response_headers_map,
+    };
+    $result->{'content_sha256'} = sha256_hex($content);
+
+    ## TODO: This should probably be functionized if used more than just these 2 times
+    # Save metadata to file in subdirectory for SHA256 sum named for the index of the URL it came from
+    my $fh = IO::File->new();
+    my $subdir_name = "$out_dir/$result->{'content_sha256'}";
+    if (! -d $subdir_name) {
+      mkdir($subdir_name) or die "Could not create the subdirectory [ $subdir_name ] to write files into: $!\n";
+    }
+    my $metafile_name = "$subdir_name/$url_index.json";
+    unless ($fh->open("$metafile_name", "w")) {
+      die "Could not open [ $metafile_name ] for writing: $!\n";
+    }
+    # Only using UTF-8 because we're JSON-ifying, if this were binary data we'd need :raw
+    $fh->binmode(':encoding(UTF-8)');
+    $fh->write(to_json($result, {utf8 => 1, pretty => 1, convert_blessed => 1, canonical => 1})) or die "Failed to write to [ $metafile_name ]: $!\n";
+
+    ## TODO: This should probably be functionized if used more than just these 2 times
+    # Save data to file in subdirectory for SHA256 sum named for the index of the URL it came from
+    my $datafile_name = "$subdir_name/$url_index.dat";
+    $fh = IO::File->new();
+    unless ($fh->open("$datafile_name", "w")) {
+      die "Could not open [ $datafile_name ] for writing: $!\n";
+      $fh->binmode(':raw');
+    }
+    $fh->write($content) or die "Failed to write to [ $datafile_name ]: $!\n";
+
+
+    $url_index++;
+  }
+}
+
+
+main();
